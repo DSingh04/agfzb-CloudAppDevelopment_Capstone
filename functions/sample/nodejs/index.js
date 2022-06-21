f22cedb6747d31a2a58c2c413b1419b7aa3b7616
DSingh04 <dushant.singh@yahoo.co.uk>
2022-06-21 08:20:55 +0000

    Changes made from theia lab

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -2,21 +2,45 @@
  * Get all dealerships
  */
 
-const Cloudant = require('@cloudant/cloudant');
+// const Cloudant = require('@cloudant/cloudant');
+
+// function main(params) {
+
+//     const cloudant = Cloudant({
+//         url: params.COUCH_URL,
+//         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+//     });
+
+//     let dbList = getDbs(cloudant);
+//     return { dbs: dbList };
+// }
+
+// function getDbs(cloudant) {
+//     cloudant.db.list().then((body) => {
+//         body.forEach((db) => {
+//             dbList.push(db);
+//         });
+//     }).catch((err) => { console.log(err); });
+// }
+
+
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
 
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
+  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+  const cloudant = CloudantV1.newInstance({
+      authenticator: authenticator
+  });
+  cloudant.setServiceUrl(params.COUCH_URL);
 
-    let dbList = getDbs(cloudant);
-    return { dbs: dbList };
+  let dbList = getDbs(cloudant);
+  return { dbs: dbList };
 }
 
 function getDbs(cloudant) {
-    cloudant.db.list().then((body) => {
+    cloudant.getAllDbs().then((body) => {
         body.forEach((db) => {
             dbList.push(db);
         });
